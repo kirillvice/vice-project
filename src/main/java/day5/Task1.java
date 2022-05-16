d91316dca7a3998fe49311c051d061f8e99fe7b0
kirill vice <kirillvice@mail.ru>
2022-05-16 07:50:51 +0300

    day 5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,51 @@
 package day5;
-
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car1 = new Car();
+        car1.setModel("");
+        car1.setColor("");
+        car1.setYear(2022);
+        System.out.println(car1.getModel());
+        System.out.println(car1.getColor());
+        System.out.println(car1.getYear());
     }
 }
+class Car {
+    private String model;
+    private String color;
+    private int year;
+
+    public void setModel(String model) {
+        if (model.isEmpty()){
+            System.out.println("Пустое имя");
+
+        }else{
+            this.model = model;
+        }
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setColor(String color) {
+        if (color.isEmpty()) {
+            System.out.println("неуказан цвет");
+        }else {
+            this.color = color;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public int getYear() {
+        return year;
+    }
+}
+
+
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,35 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+Motobike motobike1=new Motobike("green",1999,"Harley");
+System.out.println(motobike1.getColor());
+System.out.println(motobike1.getModel());
+System.out.println(motobike1.getYear());
     }
 }
+class Motobike {
+    private String color;
+    private int year;
+    private String model;
+
+
+    public Motobike(String color, int year, String model) {
+        this.color = color;
+        this.year = year;
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
+
+
